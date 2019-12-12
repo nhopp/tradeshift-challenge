62f63bf1262ab4761326dc31851177c91b46d201
nhopp <nhopper@fuze.com>
2019-12-12 00:46:17 -0800

    Add DockerFile

--- src/models/nodeInfo.ts
+++ src/models/nodeInfo.ts
@@ -1,7 +1,7 @@
 export class NodeInfo {
   /**
    *
-   * @param id id of the node
+   * @param id id of the node.
    * @param parent id of the parent node. If parent === undefined this node is the root node.
    * @param depth depth of the node. The root node depth is 0.
    * @param root id of the root node.
